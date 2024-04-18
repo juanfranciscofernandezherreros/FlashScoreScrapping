d44957b14c5d3dbddd13b2488fa863c4187a9ce7
Your Name <juanfranciscofernandezherreros>
2024-04-18 19:18:16 +0200

    Refactor results

--- src/index.js
+++ src/index.js
@@ -19,11 +19,12 @@ import { url } from "inspector";
   let headless = false;
   let action = "results";
   let ids = null;
-  let includeMatchData = true; // Default value is true
-  let includeStatsPlayer = true; // Default value is true
-  let includeStatsMatch = true; // Default value is true
-  let includePointByPoint = true; // Default value is true
-  let generateCSV = false; // Nuevo argumento para generar CSV  
+  let includeMatchData = true; 
+  let includeStatsPlayer = true; 
+  let includeStatsMatch = true; 
+  let includePointByPoint = true; 
+  let generateCSV = false;
+  let allMatchIdLists = [];
 
   process.argv?.slice(2)?.map(arg => {
     if (arg.includes("country="))
@@ -55,11 +56,62 @@ import { url } from "inspector";
     if (arg.includes("generateCSV="))
         generateCSV = arg.split("generateCSV=")?.[1]?.toLowerCase() === "true";
   });
-
+  
   if (newUrl === null || newUrl === "") {
-    console.log("New URL is not provided. It's null or empty.");
+    console.log("New URL is not provided. It's null or empty.");  
+    const browser = await puppeteer.launch({ headless });
+    console.log("Argument country ", country);
+    console.log("Argument league ", league);
+    allMatchIdLists = await getMatchIdList(browser, country, league);
+    console.log("generateCSV", generateCSV);
+    console.log("Generando archivo CSV...");
+    const fechaActual = new Date();
+    const formattedFecha = formatFecha(fechaActual);
+    const nombreArchivo = `RESULTS_${formattedFecha}_${country}_${league}`;
+    generateCSVResultsMatchs(allMatchIdLists.eventDataList, nombreArchivo);
   } else {
     console.log("New URL is provided:", newUrl);
-    // Aquí puedes agregar cualquier acción adicional que desees realizar si la URL no está vacía
+    const browser = await puppeteer.launch({ headless });
+    console.log("----");
+    const urlParts = newUrl.split("/"); // Divide la URL en partes usando "/"
+    // El país estará en la posición 4 y la liga en la posición 5, si la URL sigue un formato específico
+    const country = urlParts[4];
+    const league = urlParts[5];
+    allMatchIdLists = await getMatchIdList(browser, country, league);
+    console.log("generateCSV", generateCSV);
+    console.log("Generando archivo CSV...");
+    const fechaActual = new Date();
+    const formattedFecha = formatFecha(fechaActual);
+    const nombreArchivo = `RESULTS_${formattedFecha}_${country}_${league}`;
+    generateCSVResultsMatchs(allMatchIdLists.eventDataList, nombreArchivo);
   }
 })();
+
+function formatFecha(fecha) {
+  const year = fecha.getFullYear();
+  const month = String(fecha.getMonth() + 1).padStart(2, '0');
+  const day = String(fecha.getDate()).padStart(2, '0');
+  const hours = String(fecha.getHours()).padStart(2, '0');
+  const minutes = String(fecha.getMinutes()).padStart(2, '0');
+  const seconds = String(fecha.getSeconds()).padStart(2, '0');
+  return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
+
+function generateCSVResultsMatchs(data,nombreArchivo) {
+  if (!data || data.length === 0) {
+    console.log("No hay datos para generar el archivo CSV.");
+    return;
+  }
+
+  const headers = Object.keys(data[0]);
+  const csvContent = data.map(obj =>
+    headers.map(key => obj[key]).join(",")
+  ).join("\n");
+  const headerRow = headers.join(",") + "\n";
+  const csvData = headerRow + csvContent;
+  fs.writeFile(`${nombreArchivo}.csv`, csvData, (err) => {
+    if (err) throw err;
+    console.log('Los datos se han exportado correctamente a results.csv');
+    process.exit(0); // Termina el proceso con código de salida 0 (éxito)
+  });
+}
